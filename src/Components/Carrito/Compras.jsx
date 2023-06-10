8c08dbcb23145f1076d74ceac14eef8dac9edcb8
fedeiramain <federicoiramain@gmail.com>
2023-06-10 16:35:29 -0300

    gif

--- src/Components/Carrito/Compras.jsx
+++ src/Components/Carrito/Compras.jsx
@@ -15,7 +15,7 @@ const Compras = () => {
 
   const registro = (data) => {
     setUser(data);
-    
+
   };
 
   console.log(user)
@@ -35,7 +35,7 @@ const Compras = () => {
     const orderCollection = collection(db, "orders");
     addDoc(orderCollection, order)
       .then(({ id }) => setOrderId(id));
-  
+
     setCart([]);
     setUser([]);
   }
@@ -43,40 +43,40 @@ const Compras = () => {
   return (
     <div className="container">
       <div>
+        <div>
+          <h1>Registre sus Datos</h1>
+          <h3>Productos: {subCantidad}</h3>
+          <h3>Total: $ {subTotal}</h3>
           <div>
-            <h1>Registre sus Datos</h1>
-            <h3>Productos: {subCantidad}</h3>
-            <h3>Total: $ {subTotal}</h3>
-           <div>
+          </div>
+          <form className="form" id="formid" onSubmit={handleSubmit(registro)}>
+            <div>
+              <label className="form-datos" htmlFor="">Nombre:</label>
+              <input type="text"
+                pattern="[a-z]{4,12}"
+                {...register("nombre")}
+                placeholder='nombre' />
             </div>
-              <form className="form" id="formid" onSubmit={handleSubmit(registro)}>
-              <div>
-                <label className="form-datos" htmlFor="">Nombre:</label>
-                <input type="text"
-                  pattern="[a-z]{4,12}"
-                  {...register("nombre")}
-                  placeholder='nombre' />
-              </div>
-              <div>
-                <label className="form-datos" htmlFor="">Apellido:</label>
-                <input type="text"
-                  pattern="[a-z]{4,12}"
-                  {...register("apellido")}
-                  placeholder='apellido' />
-              </div>
-              <div>
-                <label className="form-datos" htmlFor="">E-mail:</label>
-                <input type="mail"
-                  pattern="^[a-z0-9]+@[a-z]+\.[a-z]+$"
-                  {...register("mail")}
-                  placeholder='@gmail.com' />
-              </div>
-              <button className="btn btn-success registrar" type='submit'>Registrar</button>
-            </form>
-            {user.mail && 
+            <div>
+              <label className="form-datos" htmlFor="">Apellido:</label>
+              <input type="text"
+                pattern="[a-z]{4,12}"
+                {...register("apellido")}
+                placeholder='apellido' />
+            </div>
+            <div>
+              <label className="form-datos" htmlFor="">E-mail:</label>
+              <input type="mail"
+                pattern="^[a-z0-9]+@[a-z]+\.[a-z]+$"
+                {...register("mail")}
+                placeholder='@gmail.com' />
+            </div>
+            <button className="btn btn-success registrar" type='submit'>Registrar</button>
+          </form>
+          {user.mail &&
             <Link to="/checkout">
               <button className="btn btn-success mt-3" id="pagar" onClick={handlePagar}>Pagar</button></Link>}
-          </div>
+        </div>
       </div>
     </div>
   )
